99d28cacb12e33aced489c3cfd0c1fd492988f62
minhnang <23520991@gm.uit.edu.vn>
2025-05-02 21:52:28 +0700

    react route dom...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Board from '~/pages/Boards/_id'
+import NotFound from '~/pages/404/NotFound'
+import Auth from '~/pages/Auth/Auth'
 
 function App() {
   return (
-    <>
-      {/* Routers */}
+    <Routes>
+      {/* Redirect Route */}
+      <Route path='/' element={
+        // replace gia tri true de no thay the route /, nghia la route / se kh con nam trong history cua Browser
+        <Navigate to='/boards/680c5638cd86303dd9fde5df' replace='true' />
+      } />
 
-      {/* Board details */}
-      <Board />
-    </>
+      {/* Board Details */}
+      <Route path='/boards/:boardId' element={<Board />} />
+
+      {/* Authentication */}
+      <Route path='/register' element={<Auth />} />
+      <Route path='/login' element={<Auth />} />
+
+      {/* 404 Not Found Page */}
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
